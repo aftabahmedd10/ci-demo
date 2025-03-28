7811bba61c9adafa2bd77f4a01d825dbe842c628
unknown <aftabahmed.d10@gmail.com>
2025-03-28 16:17:57 +0530

    unittest added

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 def add(a, b):
     return a + b
 
@@ -7,7 +6,15 @@ def sub(a, b):
     return a - b
 
 
+def mul(a, b):
+    return a * b
+
+
 result1 = add(10, 20)
 result2 = sub(20, 10)
 
-print(result1+result2)
+if __name__ == '__main__':
+    print(mul(10, 20))
+    print(result1+result2)
+
+
